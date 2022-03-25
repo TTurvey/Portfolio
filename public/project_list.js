8dc7d22c88dba1caac02c2542af5fc9b9cc042a9
Tim Turvey <tturvey@hotmail.co.uk>
2022-03-25 03:39:05 +0000

    Project card animations

--- public/project_list.js
+++ public/project_list.js
@@ -2,35 +2,40 @@ var projectArray = [
   {
   name : "Minesweeper",
   description : "A web based Minesweeper game.",
-  date : "",
+  date : "xx/xx/xxxx",
   techUsed : "JavaScript, HTML, CSS",
   },
   {
     name : "Chitter",
     description : "A clone of the popular Twitter app...",
-    date : "",
+    date : "xx/xx/xxxx",
     techUsed : "JavaScript, HTML, CSS",
   },
   {
     name : "Bar.io",
     description : "A bar finder app...",
-    date : "",
+    date : "xx/xx/xxxx",
     techUsed : "React, JavaScript, HTML, CSS, API's(Google Maps, Places, Geocoding)",
   }
 ];
 
-// For each array index, create a project div
+// For each array index, create a project card div and fill with projects details
 for (let i = 0; i < projectArray.length; i++) {
-  const projectsSection = document.querySelector('.projects_section');
-  const project = document.createElement('div');
-  project.setAttribute('id', i);
-  project.classList.add('project_card');
+  const projectsFrame = document.querySelector('.projects_frame');
+  const projectCard = document.createElement('div');
+  projectCard.setAttribute('id', i);
+  projectCard.classList.add('project_card');
 
-  console.log(projectArray[i])
-  
-  for (var key in projectArray[i]) { 
-    project.innerHTML += projectArray[i][key]
-    // console.log(projectArray[key]); 
-  }
-  projectsSection.appendChild(project);
-}
+  var projectName = projectArray[i]['name'];
+  var projectDescription = projectArray[i]['description'];
+  var projectDate = projectArray[i]['date'];
+  var projectTechUsed = projectArray[i]['techUsed'];
+
+  projectCard.innerHTML += `<p><span class="project_card_title">${projectName}</span></p>`;
+  projectCard.innerHTML += `<p><span class="project_card_description">${projectDescription}</span></p>`;
+  projectCard.innerHTML += `<p><span class="project_card_date">${projectDate}</span></p>`;
+  projectCard.innerHTML += `<p><span class="project_card_tools">${projectTechUsed}</span></p>`;
+  projectCard.style.backgroundImage = 'url(images/' + projectName + '.png)';
+  projectsFrame.appendChild(projectCard);
+
+};
